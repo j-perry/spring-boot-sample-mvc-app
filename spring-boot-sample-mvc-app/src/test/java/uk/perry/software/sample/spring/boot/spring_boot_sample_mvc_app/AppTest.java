21a5f72a66d67d8c1dda452babaa6f8a108a71ef
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2020-03-28 19:04:19 +0000

    Removed line and added directory

--- spring-boot-sample-mvc-app/src/test/java/uk/perry/software/sample/spring/boot/spring_boot_sample_mvc_app/AppTest.java
+++ spring-boot-sample-mvc-app/src/test/java/uk/perry/software/sample/spring/boot/spring_boot_sample_mvc_app/AppTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-
 @ExtendWith(MockitoExtension.class)
 public class AppTest {
 	
