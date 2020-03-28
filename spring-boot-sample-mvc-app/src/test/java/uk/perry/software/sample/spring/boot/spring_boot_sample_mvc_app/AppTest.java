f96292f1d0e1262130dc6a27d937d99fd3141341
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2020-03-28 18:50:09 +0000

    Added Mockito dependency and cleaned up test class

--- spring-boot-sample-mvc-app/src/test/java/uk/perry/software/sample/spring/boot/spring_boot_sample_mvc_app/AppTest.java
+++ spring-boot-sample-mvc-app/src/test/java/uk/perry/software/sample/spring/boot/spring_boot_sample_mvc_app/AppTest.java
@@ -1,38 +1,18 @@
 package uk.perry.software.sample.spring.boot.spring_boot_sample_mvc_app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+public class AppTest {
+	
+    public AppTest(String testName) {
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 }
