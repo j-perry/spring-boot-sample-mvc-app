49b93e2dd956e9ab376655f03a9fc603f037d7ad
Jonathan Perry <jonathanperry@Jonathans-MacBook-Pro.local>
2020-03-28 17:22:32 +0000

    Created basic Maven project

---
+++ spring-boot-sample-mvc-app/src/main/java/uk/perry/software/spring/boot/spring_boot_sample_mvc_app/App.java
@@ -0,0 +1,11 @@
+package uk.perry.software.spring.boot.spring_boot_sample_mvc_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+}
